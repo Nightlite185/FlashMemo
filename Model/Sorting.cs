f0e069f153b11f12d25cb789679c7f97f332d286
夜光 <cieplucharafal@gmail.com>
2025-12-20 23:26:30 +0100

    added SortingOptions enum

---
+++ Model/Sorting.cs
@@ -0,0 +1,14 @@
+namespace FlashMemo.Model
+{
+    public enum SortingOptions
+    {
+        Random,
+        Id,
+        Created,
+        LastModified,
+        NextReview,
+        LastReviewed,
+        Interval
+    }
+    public enum SortingDirection { Ascending, Descending }
+}

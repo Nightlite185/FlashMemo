1848be00381a313f06c3e27f037f8dc9a4526403
夜光 <cieplucharafal@gmail.com>
2025-12-25 17:21:27 +0100

    added IEquatable + hashcode to tag

--- Model/Tag.cs
+++ Model/Tag.cs
@@ -2,12 +2,21 @@ using System.Windows.Media;
 
 namespace FlashMemo.Model
 {
-    public class Tag(string name)
+    public class Tag(string name): IEquatable<Tag>
     {
         public int Id { get; set; }
         public string Name { get; set; } = name;
         public int UserId { get; set; }
         public ICollection<Card> Cards { get; set; } = [];
         public Color Color { get; set; }
+
+        public override bool Equals(object? obj)
+            => obj is Tag other && other.Id == this.Id;
+
+        public bool Equals(Tag? other)
+            => other is Tag t && this.Id == t.Id;
+
+        public override int GetHashCode()
+            => HashCode.Combine(Id);
     }
 }

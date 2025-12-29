1bef79bc49dea68f73a0cb753783fbaf9bbbb551
夜光 <cieplucharafal@gmail.com>
2025-12-29 20:31:48 +0100

    Added navigation buttons to MainWindow and moved it to View folder, commenting out the old one.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,27 @@
-﻿using System.Windows;
+﻿//using FlashMemo.ViewModel;
+//using System.Windows;
+//using System.Windows.Controls;
 
-namespace FlashMemo
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
-}
+//namespace FlashMemo
+//{
+//    public partial class MainWindow : Window
+//    {
+//        private readonly MainVM mainVM;
+//        public MainWindow()
+//        {
+//            InitializeComponent();
+//            mainVM = new();
+//            this.DataContext = mainVM;
+//        }
+
+//        private void DeckOptions_Click(object sender, RoutedEventArgs e)
+//        {
+//            if (sender is Button bt && bt.ContextMenu != null)
+//            {
+//                bt.ContextMenu.PlacementTarget = bt;
+//                bt.ContextMenu.IsOpen = true;
+//            }
+//        }
+
+//    }
+//}

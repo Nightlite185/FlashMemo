15cc609f5c51f37b5d76253a0de9f8b363182b42
夜光 <cieplucharafal@gmail.com>
2025-12-29 20:42:09 +0100

    feat: add DeckItemVM class and update MainVM to include Decks collection and selected deck

---
+++ ViewModel/DeckItemVM.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FlashMemo.Model;
+
+namespace FlashMemo.ViewModel
+{
+    public partial class DeckVM : ObservableObject
+    {
+        [ObservableProperty]
+        public partial bool IsSelected { get; set; }
+        
+        [ObservableProperty]
+        public partial int NewCardsCount { get; set; }
+
+        [ObservableProperty]
+        public partial int LearningCardsCount { get; set; }
+
+        [ObservableProperty]
+        public partial int ReviewCardsCount { get; set; }
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(NewCardsCount))]
+        [NotifyPropertyChangedFor(nameof(LearningCardsCount))]
+        [NotifyPropertyChangedFor(nameof(ReviewCardsCount))]
+        private partial int CardsVersion { get; set; }
+    }
+}
--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -1,9 +1,11 @@
-using CommunityToolkit.Mvvm;
+using System.Collections.ObjectModel;
 
 namespace FlashMemo.ViewModel
 {
     public class MainVM
     {
-        
+        private ObservableCollection<DeckVM> Decks { get; } = [];
+        public DeckVM? SelectedDeck { get; set; }
+        public object? CurrentView { get; set; }
     }
 }

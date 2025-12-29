e76033105c2815e984c559608716c394dae07ac9
夜光 <cieplucharafal@gmail.com>
2025-12-29 20:33:12 +0100

    feat: implement dependency injection and service configuration in App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,50 @@
 ﻿using System.Windows;
+using FlashMemo.View;
+using FlashMemo.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace FlashMemo
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static ServiceProvider SP { get; private set; } = null!;
+        private static string DbPath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            dbFileName
+        );
+        private const string dbFileName = "FlashMemo.db";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = ConfigureServices();
+            services.BuildServiceProvider();
+
+            SP.GetRequiredService<MainWindow>().Show();
+        }
+
+        private static ServiceCollection ConfigureServices()
+        {
+            ServiceCollection sc = new();
+
+            // ==== WINDOWS ====
+            sc.AddSingleton<MainWindow>();
+            sc.AddTransient<BrowseWindow>();
+            sc.AddTransient<OptionsWindow>();
+
+            // ==== VIEWMODELS ====
+            sc.AddTransient<MainVM>();
+            sc.AddTransient<OptionsVM>();
+            sc.AddTransient<BrowseVM>();
+            
+            
+            // ==== DB CONTEXT ====
+            sc.AddSqlite<FlashMemoDbContext>($"Data Source={DbPath}");
+
+            return sc;
+        }
     }
 
 }

adad40153baa9d6fac6838a64e4680b3a82f2cb0
夜光 <cieplucharafal@gmail.com>
2026-01-01 15:47:15 +0100

    added initial empty mapping methods in EntityMapper for domain and persistence entities

--- Model/EntityMapper.cs
+++ Model/EntityMapper.cs
@@ -1,7 +1,62 @@
+using FlashMemo.Model.Domain;
+using FlashMemo.Model.Persistence;
+
 namespace FlashMemo.Model
 {
     public static class EntityMapper
     {
-        
+        #region Domain to Entity
+        public static CardEntity MapToEntity(Card card)
+        {
+            
+        }
+
+        public static DeckEntity MapToEntity(Deck deck)
+        {
+            
+        }
+
+        public static SchedulerEntity MapToEntity(Scheduler scheduler)
+        {
+            
+        }
+
+        public static UserEntity MapToEntity(User User)
+        {
+            
+        }
+
+        public static TagEntity MapToEntity(Tag Tag)
+        {
+            
+        }
+        #endregion
+
+        #region Entity to Domain
+        public static Card MapToDomain(CardEntity cardEntity)
+        {
+            
+        }
+
+        public static Deck MapToDomain(DeckEntity deckEntity)
+        {
+            
+        }
+
+        public static Scheduler MapToDomain(SchedulerEntity schedulerEntity)
+        {
+            
+        }
+
+        public static User MapToDomain(UserEntity userEntity)
+        {
+            
+        }
+
+        public static Tag MapToDomain(TagEntity tagEntity)
+        {
+            
+        }
+        #endregion
     }
 }

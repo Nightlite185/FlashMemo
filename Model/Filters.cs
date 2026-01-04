246aa94d69701147918bc5c2fdd96c6db91d039c
夜光 <cieplucharafal@gmail.com>
2026-01-04 22:50:34 +0100

    filters are immutable now, and have halfway implemented ToExpression() method

--- Model/Filters.cs
+++ Model/Filters.cs
@@ -1,4 +1,5 @@
-using FlashMemo.Model.Domain;
+using System.Linq.Expressions;
+using FlashMemo.Model.Persistence;
 
 namespace FlashMemo.Model
 {   
@@ -9,17 +10,47 @@ namespace FlashMemo.Model
         Learning,
         Review
     }
-    public class Filters
+    public class Filters // all those filter props should be in BrowseVM or even FiltersVM (on class-level bc nesting + INPC is hell).
     {
-        public bool? Buried { get; set; }
-        public bool? Suspended { get; set; }
-        public List<Tag>? Tags { get; set; } = [];
-        public Deck? Deck { get; set; }
-        public CardStateFlags? State { get; set; }
-        public DateTime? Created { get; set; } // everywhere with datetime I can do like int input box with 0 meaning today, -1 yesterday, and 1 meaning tmrw, Instead of some fancy-ass datetime picker.
-        public TimeSpan? Interval { get; set; }
-        public DateTime? NextReview { get; set; }
-        public DateTime? LastReviewed { get; set; }
-        public DateTime? LastModified { get; set; }
+        public Expression<Func<CardEntity, bool>> ToExpression()
+        {
+            Expression<Func<CardEntity, bool>> query = c => true;
+
+            if (IsBuried is not null)
+                query.Combine(c => 
+                    c.IsBuried == this.IsBuried);
+
+            if (IsSuspended is not null)
+                query.Combine(c => 
+                    c.IsSuspended == this.IsSuspended);
+
+            if (IsDue is not null)
+                query.Combine(c => 
+                    c.NextReview <= DateTime.Now);
+
+            if (TagIds is not null && TagIds.Any())  
+                query.Combine(c => 
+                    TagIds.Any(t => 
+                        c.Tags.Select(t => t.Id)
+                        .Contains(t)));
+
+            if (DeckId is not null)
+                query.Combine(c => 
+                    c.DeckId == this.DeckId);
+
+            return query;
+        }
+        public bool? IsBuried { get; init; }
+        public bool? IsSuspended { get; init; }
+        public bool? IsDue { get; init; }
+        public IEnumerable<long>? TagIds { get; init; } = [];
+        public long? DeckId { get; init; }
+        public TimeSpan OverdueBy { get; init; }
+        public CardStateFlags? State { get; init; }
+        public DateTime? Created { get; init; } // everywhere with datetime I can do like int input box with 0 meaning today, -1 yesterday, and 1 meaning tmrw, Instead of some fancy-ass datetime picker.
+        public TimeSpan? Interval { get; init; }
+        public DateTime? NextReview { get; init; }
+        public DateTime? LastReviewed { get; init; }
+        public DateTime? LastModified { get; init; }
     }
 }
---
+++ Model/Helpers/ExpressionExtensions.cs
@@ -0,0 +1,18 @@
+using System.Linq.Expressions;
+
+public static class PredicateExtensions
+{
+    public static void Combine<T>(
+        this Expression<Func<T, bool>> left,
+        Expression<Func<T, bool>> right)
+    {
+        var param = Expression.Parameter(typeof(T));
+
+        var body = Expression.AndAlso(
+            Expression.Invoke(left, param),
+            Expression.Invoke(right, param)
+        );
+
+        left = Expression.Lambda<Func<T, bool>>(body, param);
+    }
+}

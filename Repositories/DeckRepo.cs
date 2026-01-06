f3b06ec224423498b1ba75c617ae8e94876f492c
夜光 <cieplucharafal@gmail.com>
2026-01-06 21:03:02 +0100

    encapsulated some shared repo things into an abstract class

--- Repositories/DeckRepo.cs
+++ Repositories/DeckRepo.cs
@@ -1,46 +1,30 @@
-using FlashMemo.Model;
-using FlashMemo.Model.Domain;
 using FlashMemo.Model.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlashMemo.Repositories
 {
-    public class DeckRepo(IDbContextFactory<AppDbContext> factory)
+    public sealed class DeckRepo(IDbContextFactory<AppDbContext> dbFactory) : RepoBase(dbFactory)
     {
-        private const string idNotFoundMessage = "deck with this id was not found in database.";
-        private readonly IDbContextFactory<AppDbContext> dbFactory = factory;
-        private DbContext? currentCtx;
-        private DeckEntity? loadedDeck = null;
-        private AppDbContext GetDb()
-        {
-            if (currentCtx != null)
-                throw new InvalidOperationException("Cannot create a second DbContext since there is already one which hasn't been disposed.");
-            
-            var db = dbFactory.CreateDbContext();
-            currentCtx = db;
-            return db;
-        }
-        
         public async Task RenameDeck(long deckId, string newName)
         {
-            var db = GetDb();
+            var db = GetDb;
             
-            var deck = await db.Decks.FindAsync(deckId) // hmm does it even track it if I just do find??? do I need to include sth here??? TO DO
-                ?? throw new ArgumentException(idNotFoundMessage, nameof(newName));
+            var deck = await db.Decks.FindAsync(deckId)
+                ?? throw new ArgumentException(IdNotFoundMsg("Deck"), nameof(newName));
             
             deck.Name = newName;
             await db.SaveChangesAsync();
         }
         public async Task AddNewDeck(DeckEntity deck)
         {
-            var db = GetDb();
+            var db = GetDb;
             
             await db.Decks.AddAsync(deck);
             await db.SaveChangesAsync();
         }
         public async Task DeleteDeck(long deckId)
         {
-            var db = GetDb();
+            var db = GetDb;
 
             var deck = await db.Decks.FindAsync(deckId) 
                 ?? throw new ArgumentException("deck with this id was not found in database.", nameof(deckId));
@@ -50,13 +34,11 @@ namespace FlashMemo.Repositories
         }
         public async Task<DeckEntity> LoadDeck(long id)
         {
-            var db = GetDb();
+            var db = GetDb;
 
             var deckEntity = await db.Decks
-                .Include(d => d.Cards)
                 .SingleAsync(d => d.Id == id);
 
-            loadedDeck = deckEntity;
             return deckEntity;
         }
     }
---
+++ Repositories/RepoBase.cs
@@ -0,0 +1,12 @@
+using FlashMemo.Model.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlashMemo.Repositories
+{
+    public abstract class RepoBase(IDbContextFactory<AppDbContext> dbFactory)
+    {
+        protected readonly IDbContextFactory<AppDbContext> dbFactory = dbFactory;
+        protected AppDbContext GetDb => dbFactory.CreateDbContext();
+        protected static string IdNotFoundMsg(string NotFound) => $"{NotFound} with this id was not found in database.";
+    }
+}

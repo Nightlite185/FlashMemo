9873ec2303bdbc5afa70fc74d0c23f89c59e61dd
夜光 <cieplucharafal@gmail.com>
2026-01-16 21:57:27 +0100

    feat: implemented DeckItemVM

--- ViewModel/DeckItemVM.cs
+++ ViewModel/DeckItemVM.cs
@@ -1,29 +1,51 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using FlashMemo.Model.Persistence;
+using FlashMemo.Services;
 
 namespace FlashMemo.ViewModel
 {
     public partial class DeckItemVM : ObservableObject, IViewModel
     {
+        public DeckItemVM(Deck deck, CardsCount cc, IEnumerable<DeckItemVM> children)
+        {
+            Deck = deck;
+            DeckId = deck.Id;
+            Name = deck.Name;
+
+            Children = [..children];
+
+            this.SetCardCount(cc);
+        }
+        
+        #region public properties
+        public long DeckId { get; init; }
+        public Deck Deck { get; init; }
+        public ObservableCollection<DeckItemVM> Children { get; set; }
+
         [ObservableProperty]
-        public partial Deck Deck { get; set; }
+        public partial string Name { get; set; }
 
         [ObservableProperty]
         public partial bool IsSelected { get; set; }
         
         [ObservableProperty]
-        public partial int NewCardsCount { get; set; }
+        public partial int LessonsCount { get; set; }
 
         [ObservableProperty]
-        public partial int LearningCardsCount { get; set; }
+        public partial int LearningCount { get; set; }
 
         [ObservableProperty]
-        public partial int ReviewCardsCount { get; set; }
-
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(NewCardsCount))]
-        [NotifyPropertyChangedFor(nameof(LearningCardsCount))]
-        [NotifyPropertyChangedFor(nameof(ReviewCardsCount))]
-        private partial int CardsVersion { get; set; }
+        public partial int ReviewsCount { get; set; }
+        #endregion
+        
+        #region methods
+        public void SetCardCount(CardsCount cc)
+        {
+            LessonsCount = cc.Lessons;
+            LearningCount = cc.Learning;
+            ReviewsCount = cc.Reviews;   
+        }
+        #endregion
     }
 }

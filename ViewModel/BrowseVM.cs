e7c8f628209932e1d30ac3e43a8634b5c99bde60
夜光 <cieplucharafal@gmail.com>
2026-01-17 22:45:33 +0100

    implemented basic properties in BrowseVM class

--- ViewModel/BrowseVM.cs
+++ ViewModel/BrowseVM.cs
@@ -1,14 +1,34 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
-using FlashMemo.Model.Persistence;
+using FlashMemo.Services;
 
 namespace FlashMemo.ViewModel
 {
     public partial class BrowseVM: ObservableObject, IViewModel
     {
+        public BrowseVM(WindowService ws, CardQueryService cqs, CardService cs, FiltersVM fVM, EditCardVM eVM)
+        {
+            cardService = cs;
+            windowService = ws;
+            cardQueryS = cqs;
+            filtersVM = fVM;
+            Cards = [];
+            editVM = eVM;
+        }
+
+        #region Public properties 
         [ObservableProperty]
         public partial ObservableCollection<CardItemVM> Cards { get; set; }
-
+        #endregion
+        #region methods
         
+        #endregion
+        #region private things
+        private readonly WindowService windowService;
+        private readonly CardQueryService cardQueryS;
+        private readonly CardService cardService;
+        private readonly FiltersVM filtersVM = null!;
+        private EditCardVM? editVM;
+        #endregion
     }
 }

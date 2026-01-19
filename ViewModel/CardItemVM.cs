319d0414595fecc3f733699d18d7851e2a158c21
夜光 <cieplucharafal@gmail.com>
2026-01-19 16:17:47 +0100

    added some derived properties off card in CardItemVM

--- ViewModel/CardItemVM.cs
+++ ViewModel/CardItemVM.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using FlashMemo.Model.Domain;
 using FlashMemo.Model.Persistence;
 
 namespace FlashMemo.ViewModel
@@ -10,5 +11,16 @@ namespace FlashMemo.ViewModel
         
         [ObservableProperty]
         public partial CardEntity Card { get; set; }
+
+        public string FrontContent => Card.FrontContent;
+        public string BackContent => Card.BackContent ?? "";
+        public Deck Deck => Card.Deck;
+        public CardState CardState => Card.State;
+        public int DayInterval => (int)Card.Interval.TotalDays;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FrontContent), nameof(BackContent), nameof(CardState))]
+        // TO DO: etc etc add more notifications in the future
+        public partial int CardVersion { get; set; }
     }
 }

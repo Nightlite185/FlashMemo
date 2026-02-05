396893e6905422db30b5ed39950c6417b6550839
夜光 <cieplucharafal@gmail.com>
2026-02-05 22:15:30 +0100

    enhanced DeckOptionsVM with new logic, integrated it with the new VM builder.

--- ViewModel/Factories/DeckOptionsMenuVMF.cs
+++ ViewModel/Factories/DeckOptionsMenuVMF.cs
@@ -1,18 +1,20 @@
 using AutoMapper;
 using FlashMemo.Model.Persistence;
 using FlashMemo.Repositories;
+using FlashMemo.Services;
 using FlashMemo.ViewModel.Windows;
 
 namespace FlashMemo.ViewModel.Factories;
 
-public class DeckOptionsVMF(IMapper m, IDeckOptionsRepo dor)
+public class DeckOptionsMenuVMF(IMapper m, IDeckOptVMBuilder doVMB, IDeckOptionsRepo dor)
 {
     private readonly IMapper mapper = m;
     private readonly IDeckOptionsRepo deckOptRepo = dor;
+    private readonly IDeckOptVMBuilder deckOptVMBuilder = doVMB;
 
     public async Task<DeckOptionsMenuVM> CreateAsync(Deck deck)
     {
-        DeckOptionsMenuVM vm = new(mapper, deckOptRepo, deck);
+        DeckOptionsMenuVM vm = new(mapper, deckOptVMBuilder, deckOptRepo, deck);
 
         await vm.InitializeAsync();
         return vm;
--- ViewModel/Windows/DeckOptionsMenuVM.cs
+++ ViewModel/Windows/DeckOptionsMenuVM.cs
@@ -7,15 +7,16 @@ using AutoMapper;
 using FlashMemo.Model.Persistence;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using FlashMemo.Services;
 
 namespace FlashMemo.ViewModel.Windows;
 
-public sealed partial class DeckOptionsMenuVM(IMapper mapper, IDeckOptionsRepo repo, Deck deck): ObservableObject//: OptionsVMBase<DeckOptions>(mapper), IViewModel
+public sealed partial class DeckOptionsMenuVM(IMapper m, IDeckOptVMBuilder doVMB, IDeckOptionsRepo dor, Deck d): ObservableObject, IViewModel
 {
     #region public properties
     [ObservableProperty]
-    public partial DeckOptionsVM OptionsVM { get; set; } = null!;
-    public ObservableCollection<DeckOptions> AllPresets { get; init; } = [];
+    public partial DeckOptionsVM CurrentOptions { get; set; } = null!;
+    public ObservableCollection<DeckOptionsVM> AllPresets { get; init; } = [];
     #endregion
 
     #region methods
@@ -23,21 +24,23 @@ public sealed partial class DeckOptionsMenuVM(IMapper mapper, IDeckOptionsRepo r
     internal async Task InitializeAsync()
     {
         AllPresets.AddRange(
-            await deckOptRepo.GetAllFromUser(deck.UserId));
+            await vmBuilder.BuildAllCounted(deck.UserId));
 
-        lastSaved = AllPresets.Single(
+        CurrentOptions = AllPresets.Single(
             p => p.Id == deck.OptionsId);
         
-        OptionsVM = mapper
-            .Map<DeckOptionsVM>(lastSaved);
+        lastSaved = mapper
+            .Map<DeckOptions>(CurrentOptions);
     }
     
     #endregion
 
     #region private things
-    private readonly Deck deck = deck;
+    private readonly Deck deck = d;
+    private readonly IMapper mapper = m;
+    private readonly IDeckOptVMBuilder vmBuilder = doVMB;
     private DeckOptions lastSaved = null!;
-    private readonly IDeckOptionsRepo deckOptRepo = repo;
+    private readonly IDeckOptionsRepo deckOptRepo = dor;
     private static DeckOptions DefaultOptions => DeckOptions.Default;
     #endregion
 
@@ -48,29 +51,48 @@ public sealed partial class DeckOptionsMenuVM(IMapper mapper, IDeckOptionsRepo r
         //* this applies any changed made in the preset,
         //* as well as syncs the relation between deck and its preset.
 
-        var saving = mapper.Map<DeckOptions>(OptionsVM);
+        var saving = mapper.Map<DeckOptions>(CurrentOptions);
 
         await deckOptRepo.SaveEditedPreset(saving);
         await deckOptRepo.AssignToDecks([deck.Id], saving.Id);
     }
 
     [RelayCommand]
-    private async Task DeletePreset()
+    private async Task DeleteCurrentPreset()
     {
         // TODO: "are you sure???" pop up show here
         
-        await deckOptRepo.Remove(OptionsVM.Id); // if accepted
+        await deckOptRepo.Remove(CurrentOptions.Id);
+        AllPresets.Remove(CurrentOptions);
+
+        // fallback to default when deleting currently viewed preset.
+        mapper.Map(DefaultOptions, CurrentOptions);
+        deck.OptionsId = -1;
     }
 
-    private async Task ClonePresetFrom(DeckOptions opt)
+    [RelayCommand]
+    private async Task DeletePresetFromList(DeckOptionsVM opt)
     {
-        var clone = opt with 
-        { 
-            Id = IdGetter.Next(), 
-            Name = $"{opt.Name} - copy"
-        };
+        if (opt.Id == CurrentOptions.Id)
+        {
+            await DeleteCurrentPreset();
+            return;
+        }
+        
+        AllPresets.Remove(opt);
+        await deckOptRepo.Remove(opt.Id);
+    }
 
-        await deckOptRepo.CreateNew(clone);
+    [RelayCommand]
+    private async Task ClonePresetFrom(DeckOptionsVM opt)
+    {
+        var clone = mapper.Map<DeckOptionsVM>(opt);
+
+        clone.Id = IdGetter.Next();
+        clone.Name = $"{opt.Name} - copy";
+
+        await deckOptRepo.CreateNew(mapper.Map<DeckOptions>(clone));
+        // todo: maybe later add mapping profile directly between vm and entity so no need for double mapping.
         
         AllPresets.Add(clone);
     }
@@ -78,27 +100,28 @@ public sealed partial class DeckOptionsMenuVM(IMapper mapper, IDeckOptionsRepo r
     [RelayCommand]
     private void ToDefault()
     {
-        mapper.Map(DefaultOptions, OptionsVM);
+        mapper.Map(DefaultOptions, CurrentOptions);
     }
 
     [RelayCommand]
     private void RevertChanges()
     {
-        mapper.Map(lastSaved, OptionsVM);
+        mapper.Map(lastSaved, CurrentOptions);
     }
 
     [RelayCommand]
-    private void ChangePreset(DeckOptionsVM chosenPreset) //? param placeholder, prob cant have params in a command.
+    private void ChangePreset(DeckOptionsVM chosenPreset)
     {
         // TODO: if any not-persisted changes made and this is called -> "are you sure?" pop-up opens.
+        //* DeckOptVM on which the ctx menu was opened goes here as "CommandParam" or whatever its called.
         
-        // TODO: figure out where the preset chosen and clicked from the list even comes from, how can I access it here
-        // todo: and intercept it right before changing, to open "are you sure?", and executing some code when user accepts to discard changes.
-        
-        OptionsVM = chosenPreset;
+        CurrentOptions.DecksAssigned--; // decrementing previous preset's deck count
+        CurrentOptions = chosenPreset;
+        CurrentOptions.DecksAssigned++; // incrementing new one's
+
         deck.OptionsId = chosenPreset.Id;
 
-        lastSaved = mapper.Map<DeckOptions>(chosenPreset);
+        mapper.Map(chosenPreset, lastSaved);
     }
     #endregion
 }

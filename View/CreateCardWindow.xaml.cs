beef6f7209b1956e3528b3e83e2eb55cc25f7f0c
夜光 <cieplucharafal@gmail.com>
2026-02-13 14:59:09 +0100

    fixed a bug where it resets the whole rich text box control instead of just flow document

--- View/CreateCardWindow.xaml.cs
+++ View/CreateCardWindow.xaml.cs
@@ -9,7 +9,6 @@ namespace FlashMemo.View
     public partial class CreateCardWindow : Window, IViewFor<CreateCardVM>
     {
         public CreateCardVM VM { get; set; } = null!;
-        private FlowDocument DefaultFlowDoc = null!;
         public CreateCardWindow()
         {
             InitializeComponent();
@@ -41,14 +40,12 @@ namespace FlashMemo.View
             await VM.AddCardCommand.ExecuteAsync(null);
 
             // clearing the editor
-            FrontBox = new(); //TODO: Clean this properly, this won't work.
-            BackBox = new();
+            FrontBox.Document = new(); //TODO: Clean this properly, this won't work.
+            BackBox.Document = new();
         }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            DefaultFlowDoc = FrontBox.Document; // TODO: MAKE THIS WORK, DEEP COPY IT SOMEHOWWW OR SERIALIZE
-
             // Load Front
             FrontBox.Document = XamlSerializer
                 .FromXaml(VM.WipCard.FrontContentXAML);
@@ -79,11 +76,11 @@ namespace FlashMemo.View
 
         private bool IsEditorEmpty()
         {
-            var front =
-                XamlSerializer.GetPlainText(FrontBox.Document);
+            var front = XamlSerializer
+                .GetPlainText(FrontBox.Document);
 
-            var back =
-                XamlSerializer.GetPlainText(BackBox.Document);
+            var back = XamlSerializer
+                .GetPlainText(BackBox.Document);
 
             return string.IsNullOrWhiteSpace(front)
                 && string.IsNullOrWhiteSpace(back);

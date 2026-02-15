df051b9b696ddd24252a0e5fd0a8bee4bbb0bdc4
夜光 <cieplucharafal@gmail.com>
2026-02-15 23:31:19 +0100

    feat: implemented CardsCountVM

---
+++ ViewModel/Wrappers/CardsCountVM.cs
@@ -0,0 +1,51 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using FlashMemo.Model.Domain;
+using FlashMemo.Model.Persistence;
+using FlashMemo.Services;
+
+public partial class CardsCountVM: ObservableObject
+{
+    public static explicit operator CardsCountVM(CardsCount cc)
+    {
+        return new()
+        {
+            Lessons = cc.Lessons,
+            Learning = cc.Learning,
+            Reviews = cc.Reviews
+        };
+    }
+    public static explicit operator CardsCountVM(CardsByState cbs)
+    {
+        return new()
+        {
+            Lessons = cbs.Lessons.Count,
+            Learning = cbs.Learning.Count,
+            Reviews = cbs.Reviews.Count
+        };
+    }
+    
+    [ObservableProperty] public partial int Lessons { get; set; }
+    [ObservableProperty] public partial int Learning { get; set; }
+    [ObservableProperty] public partial int Reviews { get; set; }
+
+    public void UpdateCount(IEnumerable<CardEntity> cards)
+    {
+        foreach (var kvp in cards.CountBy(c => c.State))
+        {
+            switch (kvp.Key)
+            {
+                case CardState.New:
+                    Lessons = kvp.Value;
+                    break;
+
+                case CardState.Learning:
+                    Learning = kvp.Value;
+                    break;
+
+                case CardState.Review:
+                    Reviews = kvp.Value;
+                    break;
+            }
+        }
+    }
+}
